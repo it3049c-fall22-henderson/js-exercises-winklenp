7a09d1aa89aa50e304459b9e3e860d262dc64efa
winklenp <55988321+winklenp@users.noreply.github.com>
2022-09-04 14:23:47 -0400

    finished arrays

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -6,50 +6,56 @@
  * indexoff(arr, 3); should return the index 2
  */
 function indexOf(arr, item) {
-  return ;
+  return arr.indexOf(item);
 }
 
 // Sum: you should be able to sum the items of an array
 // Example: arr = [1,2,3]   =>  6
 function sum(arr) {
-  return ;
+  return arr.reduce((partialSum, a) => partialSum + a, 0);
 }
 
 // Filter: Should filter out all instances of a value from an array
 // Example: filtering 2 out of [1,2,3,5,6,2,4,2] should return [1,3,5,6,4]
 function filterOut(arr, item) {
-  return ;
+  let index = arr.indexOf(item);
+  if (index >= 0) {
+    arr.splice(index, 1);
+  }
+  return arr;
 }
 
 // Append: you should be able to add an item to the end of an array
 function append(arr, item) {
-
-  return ;
+  return arr.append(item);
 }
 
 // Truncate: you should be able to remove the last item of an array
 // hint: pop, slice, splice would all work. Pay attention to the return value in the docs
 function truncate(arr) {
-  return ;
+  arr.pop();
+  return arr;
 }
 
 // Concat: you should be able to join together two arrays
 function concat(arr1, arr2) {
-  return ;
+  return arr1.concat(arr2);
 }
 
 // Insert: you should be able to add an item at the specified index of an array
 // hint: use splice and pay attention to the return value
 function insert(arr, item, index) {
-
-  return ;
+  return arr.splice(index, 0, item);
 }
 
 // Square: you should be able to square each number in an array
 // hint: use .map
 // Example: [1,2,4,5] => [1,4,16,25]
 function square(arr) {
-  return ;
+  for(num in arr){
+    num = num*num
+  }
+  return arr;
 }
 
 module.exports = {
